f5e4a3436b12f0f507ccc684244f42b8684fc136
ConnerFosterCS <61959809+ConnerFosterCS@users.noreply.github.com>
2021-03-10 21:58:31 -0800

    Added tests for multiply method

--- test_calculator.py
+++ test_calculator.py
@@ -3,3 +3,4 @@ import calculator
 class testCalculator:
     def test1(self): self.assertEqual(add(4, 2),6)
     def test2(self): self.assertEqual(sub(4, 2),2)
+    def test3(self): self.assertEqual(mul(4, 2),8)
